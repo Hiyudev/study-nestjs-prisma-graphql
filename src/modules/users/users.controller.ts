21f6c4397fbc1fd1aa64971c2daa8d57c5c8446b
Hiyudev <37001732+Hiyudev@users.noreply.github.com>
2022-09-24 12:26:01 -0300

    feat: change `users/getAll` route to `users`

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -15,7 +15,7 @@ import { UsersService } from './users.service';
 export class UsersController {
   constructor(private readonly users: UsersService) {}
 
-  @Get('/getAll')
+  @Get('/')
   async getAll() {
     return this.users.getAll();
   }
